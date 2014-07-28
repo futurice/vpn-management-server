d4c9f9028d14a0897f2a1a391a0c9b7a4b585540
Ville Tainio <tainio.ville@gmail.com>
2014-07-28 10:24:43 +0300

    Removed unnecessary url paths

--- vpn/settings.py
+++ vpn/settings.py
@@ -58,7 +58,7 @@ MEDIA_ROOT = os.path.join(os.path.dirname(__file__), "media")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
-MEDIA_URL = '/vpn/static/'
+MEDIA_URL = '/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
